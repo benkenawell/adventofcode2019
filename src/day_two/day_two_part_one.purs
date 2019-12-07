422a91f5fba5d52ea24a68485bb813bca41ee0fe
Ben Kenawell <benkenawell@gmail.com>
2019-12-07 09:38:59 +0000

    this works if the ops list didn't change

--- src/day_two/day_two_part_one.purs
+++ src/day_two/day_two_part_one.purs
@@ -2,7 +2,7 @@ module AoC.DayTwo where
   
 -- import Prelude
 import Prelude (($), (<>), (+), (*), show, Unit, bind, discard, map, pure, identity)
-import Data.Array (updateAt, drop, index)
+import Data.Array (updateAt, drop, index, take)
 import Data.Foldable (foldl)
 import Data.Maybe (maybe, Maybe(..))
 import Effect (Effect)
@@ -31,27 +31,27 @@ checkOp :: Array Int -> Maybe (Array Int)
 checkOp arr = operation arr arr
 
 startCode :: Array Int -> Array Int
-startCode input = readOps (pure input) input
+startCode input = readOps (pure input) (take 4 input) (drop 4 input)
 
-readOps :: Maybe (Array Int) -> Array Int -> Array Int
-readOps (Just allCode) [99, _] = trace ("ending...") \_ -> allCode
-readOps (Just allCode) curOp@[code, pos1, pos2, updatePos, _] = trace ("step: " <> (output curOp)) \_ -> readOps (operation allCode curOp) (drop 4 curOp)
-readOps (Just allCode) _ = trace "fell through" \_ -> allCode
-readOps Nothing _ = trace "nothing" \_ -> []
+readOps :: Maybe (Array Int) -> Array Int -> Array Int -> Array Int
+readOps (Just allCode) curOp@[99, _, _, _] _ = trace ("ending... " <> output curOp <> output allCode) \_ -> allCode -- if there are still more terms, match this one
+readOps (Just allCode) curOp@[99, _, _] _ = trace ("ending... " <> output curOp <> output allCode) \_ -> allCode -- if there are still more terms, match this one
+readOps (Just allCode) curOp@[99, _] _ = trace ("ending... " <> output curOp <> output allCode) \_ -> allCode -- if there are still more terms, match this one
+readOps (Just allCode) curOp@[99] _ = trace ("ending... " <> output curOp <> output allCode) \_ -> allCode -- if there aren't more terms, match this one
+-- the below line doesn't work because purescript doesn't allow pattern matching on variable length arrays
+readOps (Just allCode) curOp@[code, pos1, pos2, updatePos] nextOps = trace ("step: " <> (output curOp)) \_ -> readOps (operation allCode curOp) (take 4 nextOps) (drop 4 nextOps)
+readOps (Just allCode) arr _ = trace ("fell through: " <> (output arr)) \_ -> allCode
+readOps Nothing _ _ = trace "nothing" \_ -> []
     
 
 operation :: Array Int -> Array Int -> Maybe (Array Int)
-operation allCode curOp@[code, pos1, pos2, updatePos, _] = do 
+operation allCode curOp@[code, pos1, pos2, updatePos] = do 
     op1 <- index allCode pos1
     op2 <- index allCode pos2
     updateAt updatePos (opCode code op1 op2) allCode
 operation allCode _ = pure allCode
 
--- getIndex :: Array Int -> Int -> Int
--- getIndex allCode idx = maybe 0 identity $ index idx allCode
-
 opCode :: Int -> Int -> Int -> Int
-opCode 1 x y = x + y
-opCode 2 x y = x * y
-opCode z x y = 0
--- opCode 99 _ _ =
+opCode 1 y z = y + z
+opCode 2 y z = y * z
+opCode x y z = 0
