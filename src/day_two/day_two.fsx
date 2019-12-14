eb92bc57930dfc71af35fd215afb9cb703487276
Ben Kenawell <ben@benkenawell.com>
2019-12-14 12:21:43 -0800

    completed day2 part1 in fsharp

---
+++ src/day_two/day_two.fsx
@@ -0,0 +1,38 @@
+open System.IO
+
+let performOp code x y = 
+    match code with
+        | 1 -> x + y
+        | 2 -> x * y
+        | _ -> 0
+
+let readCode arr: int[] =
+    let rec readOp (code: int[]) idx = 
+        printfn "code %A %A" code.[idx] idx
+        match code.[idx] with
+            | 99 -> code
+            | _ -> Array.set code code.[idx + 3] (performOp code.[idx] code.[code.[idx+1]] code.[code.[idx+2]])
+                   readOp code (idx + 4)
+    readOp arr 0
+
+let sillyReplace input =
+    Array.set input 1 12
+    Array.set input 2 2
+    input
+
+
+let test1 = readCode [|1;0;0;0;99|] 
+printfn "%A" test1
+
+let test4 =
+    "1,1,1,4,99,5,6,0,99".Split(",")
+    |> Array.map int
+    |> readCode
+printfn "%A" test4
+
+let part1 =
+    File.ReadAllText("input.txt").Split(',')
+    |> Array.map int
+    |> sillyReplace
+    |> readCode
+    |> printfn "%A"
